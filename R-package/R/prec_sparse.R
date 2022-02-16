31e1f8526cd016b73128413d310e7b4d4ae385ee
Blunde1 <lundeberent@gmail.com>
2022-02-16 15:55:49 +0100

    Use uppercase for matrices

--- R-package/R/prec_sparse.R
+++ R-package/R/prec_sparse.R
@@ -7,7 +7,7 @@
 #'
 #' \code{prec_sparse} is the precision matrix estimator in \pkg{GraphSPME}.
 #'
-#' @param x an \eqn{nxp} matrix with \eqn{n-}observations of a \eqn{p-}vector
+#' @param X an \eqn{nxp} matrix with \eqn{n-}observations of a \eqn{p-}vector
 #' @param Z a sparse matrix indicating non-zero elements of the precision matrix
 #' @param shrinkage if shrinkage should be applied to block-frequentist-covariance see details for more
 #'
@@ -29,14 +29,14 @@
 #'
 #' @rdname prec_sparse
 #' @export
-prec_sparse <- function(x, Z, shrinkage = TRUE) {
+prec_sparse <- function(X, Z, shrinkage = TRUE) {
   # check xtype
-  if (!is.matrix(x)) {
-    stop("x must be a matrix")
+  if (!is.matrix(X)) {
+    stop("X must be a matrix")
   }
   # dimensions
-  if (!all(ncol(x) == dim(Z))) {
-    stop("ncol(x) must equal both dim(Z)")
+  if (!all(ncol(X) == dim(Z))) {
+    stop("ncol(X) must equal both dim(Z)")
   }
   # sparsity type
   if (!(class(Z) == "dgCMatrix")) {
@@ -44,5 +44,5 @@ prec_sparse <- function(x, Z, shrinkage = TRUE) {
   }
 
   # calculate precision
-  return(.prec_sparse(x, Z, shrinkage))
+  return(.prec_sparse(X, Z, shrinkage))
 }
--- R-package/tests/testthat/test_prec_sparse.R
+++ R-package/tests/testthat/test_prec_sparse.R
@@ -5,30 +5,30 @@ library(Matrix)
 
 p <- 10
 n <- 100
-x <- matrix(rnorm(p * n), nrow = n, ncol = p)
+X <- matrix(rnorm(p * n), nrow = n, ncol = p)
 Z <- bandSparse(p, p, 0:0, list(rep(1, p)))
 Z_wrong_sparsity <- as(Z, "dtCMatrix") # ensure wrong sparsity class
 Z_correct_sparsity <- as(Z, "dgCMatrix") # ensure correct sparsity class
 
 test_that("R throws an error for wrong sparsity type", {
   expect_error(
-    prec_sparse(x, Z_wrong_sparsity),
+    prec_sparse(X, Z_wrong_sparsity),
     "Z must be a sparse matrix of type dgCMatrix"
   )
 })
 
 test_that("R throws an error for wrong data input type", {
-  x_df <- data.frame(x)
+  X_df <- data.frame(x)
   expect_error(
-    prec_sparse(x_df, Z_correct_sparsity),
-    "x must be a matrix"
+    prec_sparse(X_df, Z_correct_sparsity),
+    "X must be a matrix"
   )
 })
 
 test_that("R throws an error when data and neighbourhood structure does not match", {
-  x_extended <- cbind(x, rnorm(n))
-  expect_error(prec_sparse(x_extended, Z_correct_sparsity),
-    "ncol(x) must equal both dim(Z)",
+  X_extended <- cbind(X, rnorm(n))
+  expect_error(prec_sparse(X_extended, Z_correct_sparsity),
+    "ncol(X) must equal both dim(Z)",
     fixed = TRUE
   )
 })
