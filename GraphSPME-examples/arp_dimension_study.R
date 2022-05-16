5be257acc914eb16ba876b29bbb742af05df595d
Blunde1 <lundeberent@gmail.com>
2022-05-16 11:04:52 +0200

    Add Monte-Carlo study to arp dimension study

--- GraphSPME-examples/arp_dimension_study.R
+++ GraphSPME-examples/arp_dimension_study.R
@@ -5,71 +5,85 @@
 library(ts.extend)
 library(GraphSPME)
 library(Matrix)
-library(MASS)
 library(dplyr)
 library(ggplot2)
 library(ggpubr)
+library(EigenR)
 
 options(digits=3)
 
 fbnorm <- function(M) sqrt(sum(M^2))
 
+quantileMat <- function(M, q){
+    p <- ncol(M)
+    n <- nrow(M)
+    sapply(1:p, function(j){quantile(M[,j], probs=q)})
+}
+
 set.seed(123)
+B <- 100
 
 ## INCREASING DIMENSION
 #Set the parameters
 n <- 100
 nd <- 50
-ntimes <- round(exp(seq(log(10),log(1000), length.out=nd)))
+ntimes <- round(exp(seq(log(10),log(300), length.out=nd)))
 p <- 1
 AR    <- rep(0.8,p)/p 
 
 fb_cov_s <- fb_cov_shrink <- fb_cov_le <- fb_cov_gspme <- 
-    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- numeric(nd)
-pb <- txtProgressBar(0, nd)
-for(i in 1:nd){
-    # Find population quantities
-    ntime <- ntimes[i]
-    Sigma <- ARMA.var(ntime, ar=AR)
-    Prec <- solve(Sigma)
-    G <- bandSparse(ntime, ntime, 
-                    (-p):p, 
-                    sapply((-p):p, function(order) rep(1,ntime-abs(order)))
-    )
-    # Sample data
-    xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
-    # Compute different estimates
-    S <- cov(xtr)
-    S_shrink <- cov_shrink_spd(xtr)
-    Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization = F)
-    Prec_gspme <- prec_sparse(xtr, G)
-    # Evaluate loss for covariance and precision
-    fb_cov_s[i] <- fbnorm(Sigma - S)
-    fb_cov_shrink[i] <- fbnorm(Sigma - S_shrink)
-    fb_cov_le[i] <- fbnorm(Sigma - solve(Prec_le))
-    fb_cov_gspme[i] <- fbnorm(Sigma - solve(Prec_gspme))
-    fb_prec_s[i] <- fbnorm(Prec - ginv(S))
-    fb_prec_shrink[i] <- fbnorm(Prec - solve(S_shrink))
-    fb_prec_le[i] <- fbnorm(Prec - Prec_le)
-    fb_prec_gspme[i] <- fbnorm(Prec - Prec_gspme)
-    setTxtProgressBar(pb, value=i)
+    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- matrix(nrow=B,ncol=nd)
+pb <- txtProgressBar(0, nd*B)
+for(b in 1:B){
+    for(i in 1:nd){
+        # Find population quantities
+        ntime <- ntimes[i]
+        Sigma <- ARMA.var(ntime, ar=AR)
+        Prec <- solve(Sigma)
+        G <- bandSparse(ntime, ntime, 
+                        (-p):p, 
+                        sapply((-p):p, function(order) rep(1,ntime-abs(order)))
+        )
+        # Sample data
+        xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
+        # Compute different estimates
+        S <- cov(xtr)
+        S_shrink <- cov_shrink_spd(xtr)
+        Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization = F)
+        Prec_gspme <- prec_sparse(xtr, G)
+        # Evaluate loss for covariance and precision
+        fb_cov_s[b,i] <- fbnorm(Sigma - S)
+        fb_cov_shrink[b,i] <- fbnorm(Sigma - S_shrink)
+        fb_cov_le[b,i] <- fbnorm(Sigma - solve(Prec_le))
+        fb_cov_gspme[b,i] <- fbnorm(Sigma - solve(Prec_gspme))
+        fb_prec_s[b,i] <- fbnorm(Prec - Eigen_pinverse(S))
+        fb_prec_shrink[b,i] <- fbnorm(Prec - solve(S_shrink))
+        fb_prec_le[b,i] <- fbnorm(Prec - Prec_le)
+        fb_prec_gspme[b,i] <- fbnorm(Prec - Prec_gspme)
+        setTxtProgressBar(pb, value=(nd-1)*b + i)
+    }   
 }
 close(pb)
 
 df_cov <- data.frame(
     "dimension" = rep(ntimes, 4),
-    "frobenius" = c(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme),
+    "frobenius" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), colMeans)),
+    "low" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("sample",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 df_prec <- data.frame(
     "dimension" = rep(ntimes, 4),
-    "frobenius" = c(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme),
+    "frobenius" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), colMeans)),
+    "low" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("SCV",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 p_fb_cov <- 
     df_cov %>% 
     ggplot(aes(x=dimension, y=frobenius, group=Estimate)) + 
     geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Covariance matrix"
     ) + 
@@ -80,7 +94,8 @@ p_fb_cov <-
 p_fb_prec <- 
     df_prec %>% 
     ggplot(aes(x=dimension, y=frobenius, group=Estimate)) + 
-    geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_line(aes(colour=Estimate, linetype=Estimate), size=1) +
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Precision matrix"
     ) + 
@@ -105,52 +120,58 @@ if(F){
 ## INCREASING SAMPLE SIZE
 #Set the parameters
 nd <- 50
-ns <- round(exp(seq(log(30),log(100000), length.out=nd)))
+ns <- round(exp(seq(log(30),log(10000), length.out=nd)))
 ntime <- 100
 #ntimes <- round(exp(seq(log(10),log(1000), length.out=nd)))
 p <- 1
 AR    <- rep(0.8,p)/p 
 
 fb_cov_s <- fb_cov_shrink <- fb_cov_le <- fb_cov_gspme <- 
-    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- numeric(nd)
-pb <- txtProgressBar(0, nd)
-for(i in 1:nd){
-    # Find population quantities
-    n <- ns[i]
-    Sigma <- ARMA.var(ntime, ar=AR)
-    Prec <- solve(Sigma)
-    G <- bandSparse(ntime, ntime, 
-                    (-p):p, 
-                    sapply((-p):p, function(order) rep(1,ntime-abs(order)))
-    )
-    # Sample data
-    xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
-    # Compute different estimates
-    S <- cov(xtr)
-    S_shrink <- cov_shrink_spd(xtr)
-    Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization = F)
-    Prec_gspme <- prec_sparse(xtr, G)
-    # Evaluate loss for covariance and precision
-    fb_cov_s[i] <- fbnorm(Sigma - S)
-    fb_cov_shrink[i] <- fbnorm(Sigma - S_shrink)
-    fb_cov_le[i] <- fbnorm(Sigma - solve(Prec_le))
-    fb_cov_gspme[i] <- fbnorm(Sigma - solve(Prec_gspme))
-    fb_prec_s[i] <- fbnorm(Prec - ginv(S))
-    fb_prec_shrink[i] <- fbnorm(Prec - solve(S_shrink))
-    fb_prec_le[i] <- fbnorm(Prec - Prec_le)
-    fb_prec_gspme[i] <- fbnorm(Prec - Prec_gspme)
-    setTxtProgressBar(pb, value=i)
+    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- matrix(nrow=B, ncol=nd)
+pb <- txtProgressBar(0, B*nd)
+for(b in 1:B){
+    for(i in 1:nd){
+        # Find population quantities
+        n <- ns[i]
+        Sigma <- ARMA.var(ntime, ar=AR)
+        Prec <- solve(Sigma)
+        G <- bandSparse(ntime, ntime, 
+                        (-p):p, 
+                        sapply((-p):p, function(order) rep(1,ntime-abs(order)))
+        )
+        # Sample data
+        xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
+        # Compute different estimates
+        S <- cov(xtr)
+        S_shrink <- cov_shrink_spd(xtr)
+        Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization = F)
+        Prec_gspme <- prec_sparse(xtr, G)
+        # Evaluate loss for covariance and precision
+        fb_cov_s[b,i] <- fbnorm(Sigma - S)
+        fb_cov_shrink[b,i] <- fbnorm(Sigma - S_shrink)
+        fb_cov_le[b,i] <- fbnorm(Sigma - solve(Prec_le))
+        fb_cov_gspme[b,i] <- fbnorm(Sigma - solve(Prec_gspme))
+        fb_prec_s[b,i] <- fbnorm(Prec - Eigen_pinverse(S))
+        fb_prec_shrink[b,i] <- fbnorm(Prec - solve(S_shrink))
+        fb_prec_le[b,i] <- fbnorm(Prec - Prec_le)
+        fb_prec_gspme[b,i] <- fbnorm(Prec - Prec_gspme)
+        setTxtProgressBar(pb, value=(nd-1)*b + i)
+    }   
 }
 close(pb)
 
 df_cov <- data.frame(
-    "sample-size" = rep(ns, 4),
-    "frobenius" = c(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme),
+    "sample_size" = rep(ns, 4),
+    "frobenius" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), colMeans)),
+    "low" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("sample",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 df_prec <- data.frame(
-    "sample-size" = rep(ns, 4),
-    "frobenius" = c(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme),
+    "sample_size" = rep(ns, 4),
+    "frobenius" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), colMeans)),
+    "low" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("SCV",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 names(df_cov)[1] <- names(df_prec)[1] <- "sample-size"
@@ -158,6 +179,7 @@ p_fb_cov <-
     df_cov %>% 
     ggplot(aes(x=`sample-size`, y=frobenius, group=Estimate)) + 
     geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Covariance matrix"
     ) + 
@@ -170,6 +192,7 @@ p_fb_prec <-
     df_prec %>% 
     ggplot(aes(x=`sample-size`, y=frobenius, group=Estimate)) + 
     geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Precision matrix"
     ) + 
@@ -200,51 +223,57 @@ ps <- seq(0, ntime-1)
 nd <- length(ps)
 
 fb_cov_s <- fb_cov_shrink <- fb_cov_le <- fb_cov_gspme <- 
-    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- numeric(nd)
-pb <- txtProgressBar(0, nd)
-for(i in 1:nd){
-    # Find population quantities
-    p <- ps[i]
-    if(p>0){
-        AR    <- rep(0.8,p)/p    
-    }else{
-        AR <- numeric(0)
-    }
-    Sigma <- ARMA.var(ntime, ar=AR)
-    Prec <- solve(Sigma)
-    G <- bandSparse(ntime, ntime, 
-                    (-p):p, 
-                    sapply((-p):p, function(order) rep(1,ntime-abs(order)))
-    )
-    G <- as(G, "dgCMatrix")
-    # Sample data
-    xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
-    # Compute different estimates
-    S <- cov(xtr)
-    S_shrink <- cov_shrink_spd(xtr)
-    Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization=F)
-    Prec_gspme <- prec_sparse(xtr, G)
-    # Evaluate loss for covariance and precision
-    fb_cov_s[i] <- fbnorm(Sigma - S)
-    fb_cov_shrink[i] <- fbnorm(Sigma - S_shrink)
-    fb_cov_le[i] <- fbnorm(Sigma - solve(Prec_le))
-    fb_cov_gspme[i] <- fbnorm(Sigma - solve(Prec_gspme))
-    fb_prec_s[i] <- fbnorm(Prec - ginv(S))
-    fb_prec_shrink[i] <- fbnorm(Prec - solve(S_shrink))
-    fb_prec_le[i] <- fbnorm(Prec - Prec_le)
-    fb_prec_gspme[i] <- fbnorm(Prec - Prec_gspme)
-    setTxtProgressBar(pb, value=i)
+    fb_prec_s <- fb_prec_shrink <- fb_prec_le <- fb_prec_gspme <- 
+    matrix(nrow=B,ncol=nd)
+pb <- txtProgressBar(0, B*nd)
+for(b in 1:B){
+    for(i in 1:nd){
+        # Find population quantities
+        p <- ps[i]
+        if(p>0){
+            AR    <- rep(0.8,p)/p    
+        }else{
+            AR <- numeric(0)
+        }
+        Sigma <- ARMA.var(ntime, ar=AR)
+        Prec <- solve(Sigma)
+        G <- bandSparse(ntime, ntime, 
+                        (-p):p, 
+                        sapply((-p):p, function(order) rep(1,ntime-abs(order)))
+        )
+        G <- as(G, "dgCMatrix")
+        # Sample data
+        xtr <- ts.extend::rGARMA(n,ntime, ar=AR)
+        # Compute different estimates
+        S <- cov(xtr)
+        S_shrink <- cov_shrink_spd(xtr)
+        Prec_le <- prec_sparse(xtr, G, shrinkage=F, symmetrization=F)
+        Prec_gspme <- prec_sparse(xtr, G)
+        # Evaluate loss for covariance and precision
+        fb_cov_s[b,i] <- fbnorm(Sigma - S)
+        fb_cov_shrink[b,i] <- fbnorm(Sigma - S_shrink)
+        fb_cov_le[b,i] <- fbnorm(Sigma - solve(Prec_le))
+        fb_cov_gspme[b,i] <- fbnorm(Sigma - solve(Prec_gspme))
+        fb_prec_s[b,i] <- fbnorm(Prec - Eigen_pinverse(S))
+        fb_prec_shrink[b,i] <- fbnorm(Prec - solve(S_shrink))
+        fb_prec_le[b,i] <- fbnorm(Prec - Prec_le)
+        fb_prec_gspme[b,i] <- fbnorm(Prec - Prec_gspme)
+        setTxtProgressBar(pb, value=(nd-1)*b+i)
+    }   
 }
 close(pb)
-
 df_cov <- data.frame(
-    "auto-regressive parameter" = rep(ps, 4),
-    "frobenius" = c(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme),
+    "sample_size" = rep(ps, 4),
+    "frobenius" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), colMeans)),
+    "low" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_cov_s, fb_cov_shrink, fb_cov_le, fb_cov_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("sample",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 df_prec <- data.frame(
-    "auto-regressive parameter" = rep(ps, 4),
-    "frobenius" = c(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme),
+    "sample_size" = rep(ps, 4),
+    "frobenius" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), colMeans)),
+    "low" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.05)),
+    "high" = c(sapply(list(fb_prec_s, fb_prec_shrink, fb_prec_le, fb_prec_gspme), quantileMat, q=0.95)),
     "Estimate" = c(rep("SCV",nd), rep("shrinkage",nd), rep("Le",nd), rep("gspme",nd))
 )
 names(df_cov)[1] <- names(df_prec)[1] <- "auto-regressive parameter"
@@ -252,6 +281,7 @@ p_fb_cov <-
     df_cov %>% 
     ggplot(aes(x=`auto-regressive parameter`, y=frobenius, group=Estimate)) + 
     geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Covariance matrix"
     ) + 
@@ -263,6 +293,7 @@ p_fb_prec <-
     df_prec %>% 
     ggplot(aes(x=`auto-regressive parameter`, y=frobenius, group=Estimate)) + 
     geom_line(aes(colour=Estimate, linetype=Estimate), size=1) + 
+    geom_ribbon(aes(ymin=low, ymax=high, fill=Estimate), alpha=0.2) + 
     ggtitle(
         "Precision matrix"
     ) + 
