bb7e10691412b0ebebd020cee26b3759ad6326a2
Blunde1 <lundeberent@gmail.com>
2023-07-17 13:57:40 +0200

    trying to format with black without args

--- python-package/src/graphspme/__init__.py
+++ python-package/src/graphspme/__init__.py
@@ -85,15 +85,11 @@ def chol_to_precision(
     return _chol_to_precision(L, perm_indices)
 
 
-def dmrf(
-    X: NDArray, Prec: scipy.sparse.csr_matrix, perm_indices: NDArray
-) -> float:
+def dmrf(X: NDArray, Prec: scipy.sparse.csr_matrix, perm_indices: NDArray) -> float:
     return _dmrf(X, Prec, perm_indices)
 
 
-def dmrfL(
-    X: NDArray, Prec: scipy.sparse.csr_matrix, perm_indices: NDArray
-) -> float:
+def dmrfL(X: NDArray, Prec: scipy.sparse.csr_matrix, perm_indices: NDArray) -> float:
     return _dmrfL(X, Prec, perm_indices)
 
 
